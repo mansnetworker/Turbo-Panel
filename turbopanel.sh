cf8daec3f4d226b7b4a4e6f478592a22eba745c9
mans <117917052+mansnetworker@users.noreply.github.com>
2025-04-10 08:42:02 +0330

    Update turbopanel.sh

--- turbopanel.sh
+++ turbopanel.sh
@@ -11,7 +11,8 @@ echo "--------------------------"
 echo "Choose an option:"
 echo "1) Install"
 echo "2) Uninstall"
-read -p "Enter your choice (1 or 2): " choice
+echo "3) Update"
+read -p "Enter your choice (1, 2 or 3): " choice
 
 if [[ "$choice" == "1" ]]; then
     echo "Updating and upgrading system..."
@@ -52,6 +53,31 @@ elif [[ "$choice" == "2" ]]; then
 
     echo "X-UI has been removed successfully."
 
+elif [[ "$choice" == "3" ]]; then
+    echo "Updating X-UI to latest version..."
+
+    docker stop x-ui
+    docker rm x-ui
+    docker rmi -f alireza7/x-ui:latest
+
+    docker pull alireza7/x-ui:latest
+
+    if [ -d x-ui ]; then
+        cd x-ui
+    else
+        mkdir x-ui && cd x-ui
+    fi
+
+    docker run -itd \
+    -p 54321:54321 -p 2052:2052 -p 80:80 \
+    -e XRAY_VMESS_AEAD_FORCED=false \
+    -v $PWD/db/:/etc/x-ui/ \
+    -v $PWD/cert/:/root/cert/ \
+    --name x-ui --restart=unless-stopped \
+    alireza7/x-ui:latest
+
+    echo "X-UI updated successfully!"
+
 else
-    echo "Invalid choice. Please run the script again and select 1 or 2."
+    echo "Invalid choice. Please run the script again and select 1, 2 or 3."
 fi
